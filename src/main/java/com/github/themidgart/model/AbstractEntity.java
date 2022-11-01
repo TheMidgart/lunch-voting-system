22d9d24d0c464124b00aa59ca7b049399e07ae1b
TheMidgart <TheMidgart@gmail.com>
2022-11-01 16:13:56 +0300

    create git repo and add structure of project

---
+++ src/main/java/com/github/themidgart/model/AbstractEntity.java
@@ -0,0 +1,53 @@
+package com.github.themidgart.model;
+
+import java.util.Objects;
+
+public abstract class AbstractEntity {
+
+    public static final int START_SEQ = 100000;
+
+    protected Integer id;
+
+    protected AbstractEntity() {
+    }
+
+    protected AbstractEntity(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ":" + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AbstractEntity that = (AbstractEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
+}
+
---
+++ src/main/java/com/github/themidgart/model/Users.java
@@ -0,0 +1,43 @@
+package com.github.themidgart.model;
+
+public class Users extends AbstractEntity{
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private boolean isAdmin;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
