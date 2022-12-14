a5bc049534bb1cefbe2fe681d41bee227d1f26b3
Midgart <TheMidgart@gmail.com>
2022-12-14 22:15:39 +0300

    add admin controller with crud

--- src/main/java/com/github/themidgart/service/UserService.java
+++ src/main/java/com/github/themidgart/service/UserService.java
@@ -1,15 +1,18 @@
 package com.github.themidgart.service;
 
+import com.github.themidgart.exception.NotFoundException;
 import com.github.themidgart.model.User;
 import com.github.themidgart.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
     @Autowired
     private UserRepository repository;
@@ -24,6 +27,16 @@ public class UserService {
 
     public User save(User user){
         return repository.save(user);
+    }
+
+    @Transactional
+    public User update(int id, User user){
+        if (repository.existsById(id)){
+            return repository.save(user);
+        }
+        else {
+            throw new NotFoundException("User with id "+id+ " not found");
+        }
     }
 
     public void delete(int id){
