7c5df3a53c7b2b065032c3d1e0922bc4c1c8949f
TheMidgart <themidgart@gmail.com>
2022-12-28 14:28:07 +0300

    add security

--- src/main/java/com/github/themidgart/service/UserService.java
+++ src/main/java/com/github/themidgart/service/UserService.java
@@ -3,17 +3,25 @@ package com.github.themidgart.service;
 import com.github.themidgart.exception.NotFoundException;
 import com.github.themidgart.model.User;
 import com.github.themidgart.repository.UserRepository;
+import com.github.themidgart.security.AuthUser;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Primary
 @Service
 @AllArgsConstructor
+@Slf4j
 @Transactional(readOnly = true)
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository repository;
 
@@ -41,5 +49,13 @@ public class UserService {
 
     public void delete(int id){
         repository.deleteById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("trying authorize with email {}",email);
+        User user = repository.findByEmailIgnoreCase(email).orElseThrow(
+                () -> new UsernameNotFoundException(String.format("Email %s not found", email)));
+        return new AuthUser(user);
     }
 }
