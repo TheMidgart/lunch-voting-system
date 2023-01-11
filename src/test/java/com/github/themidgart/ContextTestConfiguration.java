79c27dbd9be5c82c010f4495feea7664ff6b183a
Midgart <TheMidgart@gmail.com>
2023-01-11 22:21:07 +0300

    add TimingExtension

--- src/test/java/com/github/themidgart/ContextTestConfiguration.java
+++ src/test/java/com/github/themidgart/ContextTestConfiguration.java
@@ -1,9 +1,11 @@
 package com.github.themidgart;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ExtendWith(TimingExtension.class)
 public abstract class ContextTestConfiguration {
 }
