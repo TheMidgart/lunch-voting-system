1ab718ab7f132c5166b650faa441d34d18c29d9e
Midgart <TheMidgart@gmail.com>
2023-01-14 23:48:36 +0300

    fix voting tests on another platform

--- src/test/java/com/github/themidgart/VotingTestData.java
+++ src/test/java/com/github/themidgart/VotingTestData.java
@@ -6,8 +6,6 @@ import com.github.themidgart.to.VotingResultTo;
 import java.time.LocalDate;
 import java.util.HashMap;
 
-import static com.github.themidgart.MenuTestData.MENU_2;
-import static com.github.themidgart.MenuTestData.MENU_TO_VOTE;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class VotingTestData {
@@ -21,8 +19,6 @@ public class VotingTestData {
     public static final LocalDate TODAY = LocalDate.now();
     public static final LocalDate TOMORROW = LocalDate.now().plusDays(1);
     public static final int VOTING_ID = 100022;
-    public static final VotingResult VOTING_RESULT = new VotingResult(VOTING_ID, UserTestData.USER, MENU_TO_VOTE);
-    public static final VotingResult VOTING_CHANGING_RESULT = new VotingResult(VOTING_ID, UserTestData.USER, MENU_2);
     public static final HashMap<String, Long> map = new HashMap<>();
 
     static {
