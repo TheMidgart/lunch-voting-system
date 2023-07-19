202244d311843e5ef325a665095a142c061353a2
TheMidgart <TheMidgart@gmail.com>
2023-07-19 20:20:59 +0300

    LVS-4 improve LoggableAspect logic

--- src/main/java/com/github/themidgart/util/logger/LoggableAspect.java
+++ src/main/java/com/github/themidgart/util/logger/LoggableAspect.java
@@ -5,34 +5,44 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 @Component
 @Aspect
 @Slf4j
 public class LoggableAspect {
+    private record ParamForLog(String fieldName, String fieldValue, String objectName) {
+        @Override
+        public String toString() {
+            return "логируемое поле - " + fieldName +
+                    " cо значением - " + fieldValue +
+                    " объекта " + objectName;
+        }
+    }
+
     @Around("@annotation(loggable)")
     public Object logMethodParams(ProceedingJoinPoint joinPoint, Loggable loggable) throws Throwable {
+        HashSet<String> loggableFields = new HashSet<>(List.of(loggable.loggableFields()));
+        Object[] inputObjects = joinPoint.getArgs();
+        List<ParamForLog> fieldParams = new ArrayList<>();
+
         try {
-            HashSet<String> loggableFields = new HashSet<>(List.of(loggable.loggableFields()));
-            Object[] objects = joinPoint.getArgs();
-            Map<String, String> objectFields = new HashMap<>();
-            Arrays.stream(joinPoint.getArgs())
-                    .map(Object::getClass)
-                    .map(Class::getDeclaredFields)
-                    .flatMap(Arrays::stream)
-                    .filter(field -> loggableFields.contains(field.getName()))
-                    .forEach(field -> {
+            for (int i = 0; i < inputObjects.length; i++) {
+                int finalI = i;
+                ReflectionUtils.doWithFields(inputObjects[i].getClass(), field -> {
+                    if (loggableFields.contains(field.getName())) {
                         field.setAccessible(true);
-                        try {
-                            objectFields.put(field.getName(), field.get(objects[0]).toString());
-                        } catch (IllegalAccessException e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
-            log.info("Началась обработка запроса со в методе {} co следующими параметрами {}",
-                    joinPoint.getSignature(), objectFields);
+                        fieldParams.add(new ParamForLog(field.getName(),
+                                field.get(inputObjects[finalI]).toString(),
+                                inputObjects[finalI].getClass().toString()));
+                    }
+                });
+            }
+            log.info("Началась обработка метода  {} с полями {}", joinPoint.getSignature(), fieldParams);
         } catch (Exception ignored) {
             log.warn("Не удалось логгировать параметры при вызове метода {}", joinPoint.getSignature());
         }
